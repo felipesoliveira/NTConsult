8db58e269705f43974b96d7750fce2a3186e332b
felipesoliveira <sfofelipe@gmail.com>
2024-05-10 12:37:49 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "api_image" {
+  description = "Docker image for the API application"
+}
+
+# Add more variables as needed
